0b23725d6875fedb9e49c8977870b4ad7b071f3c
peergrammer <phdwight@gmail.com>
2020-10-26 18:59:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# build_of_legends_ml
+ Mobile Legends itemization guide
---
+++ android/app/src/main/kotlin/com/example/build_of_legends_ml/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.build_of_legends_ml
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
